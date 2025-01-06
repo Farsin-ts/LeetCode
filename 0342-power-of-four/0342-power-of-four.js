31d4808d0af96914c93ad6517f2a6db9764665f2
Farsin-ts <164225644+Farsin-ts@users.noreply.github.com>
2025-01-06 10:43:09 +0530

    Time: 0 ms (100%), Space: 52.2 MB (85.82%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,8 @@
+function isPowerOfFour(n) {
+    if (n <= 0) return false; 
+    while (n > 1) {
+        if (n % 4 !== 0) return false; 
+        n = Math.floor(n / 4); 
+    }
+    return true;
+}
