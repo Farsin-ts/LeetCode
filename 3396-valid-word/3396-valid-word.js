c7b851349756a57a082e09b969404aae30104f14
Farsin-ts <164225644+Farsin-ts@users.noreply.github.com>
2025-03-15 09:07:23 +0530

    Time: 1 ms (77.33%), Space: 54.5 MB (32%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.js
@@ -0,0 +1,22 @@
+function isValid(word) {
+    if (word.length < 3) {
+        return false;
+    }
+
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    let hasVowel = false;
+    let hasConsonant = false;
+
+    for (let char of word) {
+        if (!/[a-zA-Z0-9]/.test(char)) {
+            return false; 
+        }
+        if (vowels.has(char)) {
+            hasVowel = true; 
+        } else if (/[a-zA-Z]/.test(char)) {
+            hasConsonant = true; 
+        }
+    }
+
+    return hasVowel && hasConsonant;
+}
